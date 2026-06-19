441da091f617c0aafbced68d6fdd9751e69d568e
long <ab1234_1123@qq.com>
2026-06-19 16:07:08 +0800

    feat add some message create method

--- tcp/Message/Message.go
+++ tcp/Message/Message.go
@@ -1,10 +1,12 @@
 package Message
 
+import "encoding/json"
+
 type Message struct {
 	t    MsgType
 	key  uint64
 	len  int
-	data []byte
+	Data []byte
 }
 
 func NewMessage(t MsgType, key uint64, data []byte) *Message {
@@ -12,7 +14,7 @@ func NewMessage(t MsgType, key uint64, data []byte) *Message {
 		t:    t,
 		key:  key,
 		len:  len(data),
-		data: nil,
+		Data: nil,
 	}
 }
 func AckMessage(key uint64) *Message {
@@ -20,4 +22,20 @@ func AckMessage(key uint64) *Message {
 }
 func HeartMessage(key uint64) *Message {
 	return NewMessage(HeartBeat, key, nil)
+}
+func JsonMessage(key uint64, target any) (*Message, error) {
+	data, err := json.Marshal(target)
+	if err != nil {
+		return nil, err
+	}
+	return NewMessage(Json, key, data), nil
+}
+func TextMessage(key uint64, text string) *Message {
+	return NewMessage(Text, key, []byte(text))
+}
+func BlobMessage(key uint64, blob []byte) *Message {
+	return NewMessage(Blob, key, blob)
+}
+func (m *Message) Len() int {
+	return m.len
 }
